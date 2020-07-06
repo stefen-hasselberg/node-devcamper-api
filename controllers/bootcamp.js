ed8537bf8e8c6efeb152b5c689dbeaab09b7b5c7
Stefen Hasselberg <stefen74@gmail.com>
2020-07-07 06:38:46 +1200

    have setup controllers

---
+++ controllers/bootcamp.js
@@ -0,0 +1,40 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "show all bootcamps" });
+};
+
+// @desc    Get single bootcamps
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "show all bootcamps" });
+};
+
+// @desc    Create new  bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: "create new bootcamp from Controller" });
+};
+
+// @desc    Update bootcamp
+// @route   Put /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `update bootcamp with id ${req.params.id}` });
+};
+
+// @desc    Delete bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamp with id ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+
+const bootcamps = require("./routes/bootcamps");
+
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
 // instansate a express server
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// moute routers
+app.use("/api/v1/bootcamps", bootcamps);
 
-console.log("config ", process.env.PORT);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
